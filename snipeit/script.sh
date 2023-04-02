64cfa2a4cc7277e35931a45ebf6ac31f4d25883e
tamld <ductam1828@gmail.com>
2023-04-02 16:43:28 +0700

    add ansible snipeit with playbook

--- snipeit/script.sh
+++ snipeit/script.sh
@@ -26,7 +26,7 @@
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
-  apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+  apt-get install -y docker.io docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
   ##Manage Docker as a non-root user
   groupadd docker
   usermod -aG docker vagrant
